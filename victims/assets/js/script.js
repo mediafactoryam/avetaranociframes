1170b45042190614422f127c420ee4c7394822f8
hanuman <a.kolyan@gratun.am>
2021-07-27 18:50:53 +0400

    init

---
+++ victims/assets/js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  const ROOT_EL = $("#root");
+  $(`<div class="flip-container">
+      <h1>${INFO.title}</h1>
+      <p>${INFO.text}</p>
+  </div>`).appendTo($(".with-title"));
+  for (const { name, dateOfDeath, description, link, photo } of DATA) {
+    $(`
+    <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+	    <div class="flipper">
+		    <div class="front">
+          <img src="${photo}" alt="${name}" />
+          <div>
+            <h2>${name}</h2>
+            <p>${dateOfDeath}</p>
+          </div>
+        </div>
+        <div class="back">
+          <p>${description}</p>
+          <a href="${link}" target="_blank" >Կարդալ ավելին</a>
+        </div>
+      </div>
+    </div>
+    `).appendTo(ROOT_EL);
+  }
+});
